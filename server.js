6dd4d6187ff84ef0549971cd4b2e9b0e30574ff6
henriqueeprog22 <droidgames60@gmail.com>
2022-06-25 15:28:40 -0300

    okok

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-var connection = mysql.createConnection({
+var db_config = {
 host: 'mysql746.umbler.com',
 user: 'gaming_proplan1',
 password:'mustang300gt',
@@ -17,9 +17,36 @@ database: 'gaming_proplan',
 port: 41890,
 ssl: true
 
-});
+};
 
+var connection;
 
+function handleDisconnect() {
+  
+    connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                    // the old one cannot be reused.
+  
+  /* connection.connect(function(err) {              // The server is either down
+       if(err) {                                     // or restarting (takes a while sometimes).
+        console.log('error when connecting to db:', err);
+         setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+        }                                     // to avoid a hot loop, and to allow our node script to
+      });                                     // process asynchronous requests in the meantime.
+    */
+      setTimeout(handleDisconnect, 2000);
+      // If you're also serving http, display a 503 error.
+      connection.on('error', function(err) {
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+          handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+          throw err;                                  // server variable configures this)
+        }
+      });
+    }
+    
+    handleDisconnect();
+    
 
 
 connection.connect(function(err){
